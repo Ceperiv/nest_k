8e41d7c4466a3d2079daa1e2fd2c5d554e23b287
Andriy Konoval <ceperiv@gmail.com>
2023-12-21 16:26:37 +0200

    super({ usernameField: 'email' });  change input auth key

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -7,7 +7,7 @@ import { AuthService } from './auth.service';
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
-    super();
+    super({ usernameField: 'email' });
   }
 
   async validate(email: string, pass: string): Promise<any> {
