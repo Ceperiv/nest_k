cbd767075ba9654165456584e011ad039479e684
Andriy Konoval <ceperiv@gmail.com>
2023-12-21 20:21:24 +0200

    dto committed

--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,37 +1,37 @@
-import {
-  IsBoolean,
-  IsEmail,
-  IsNumber,
-  IsOptional,
-  IsString,
-  Length,
-} from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class UserDto {
-  id?: number;
-
-  @ApiProperty({
-    required: true,
-    example: 'Ivan',
-  })
-  @IsString()
-  @Length(2)
-  name: string;
-
-  @IsNumber()
-  @IsOptional()
-  age: number;
-
-  @IsString()
-  @IsEmail()
-  email: string;
-
-  @IsString()
-  @IsOptional()
-  gender: string;
-
-  @IsBoolean()
-  @IsOptional()
-  isAdmin: boolean;
-}
+// import {
+//   IsBoolean,
+//   IsEmail,
+//   IsNumber,
+//   IsOptional,
+//   IsString,
+//   Length,
+// } from 'class-validator';
+// import { ApiProperty } from '@nestjs/swagger';
+//
+// export class UserDto {
+//   id?: number;
+//
+//   @ApiProperty({
+//     required: true,
+//     example: 'Ivan',
+//   })
+//   @IsString()
+//   @Length(2)
+//   name: string;
+//
+//   @IsNumber()
+//   @IsOptional()
+//   age: number;
+//
+//   @IsString()
+//   @IsEmail()
+//   email: string;
+//
+//   @IsString()
+//   @IsOptional()
+//   gender: string;
+//
+//   @IsBoolean()
+//   @IsOptional()
+//   isAdmin: boolean;
+// }
