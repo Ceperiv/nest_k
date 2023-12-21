0d2649a990de912d1d928b258c596310268fa87f
Andriy Konoval <ceperiv@gmail.com>
2023-12-21 17:21:01 +0200

    problem fixed. added JwtStrategy into providers:

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,18 +6,19 @@ import { UserModule } from '../users/user.module';
 import { LocalStrategy } from './local.strategy';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     UserModule,
     PassportModule,
     JwtModule.register({
-      secret: 'secret_key',
-      signOptions: { expiresIn: '60s' },
+      secret: 'wwwwl md;fkmdlf',
+      signOptions: { expiresIn: '1d' },
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy, JwtService],
+  providers: [AuthService, LocalStrategy, JwtService, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
